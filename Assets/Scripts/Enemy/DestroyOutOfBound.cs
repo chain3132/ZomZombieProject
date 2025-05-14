d8059d5dc62ad1876192038b45b3a6afe032ebe9
chain3132 <148683922+chain3132@users.noreply.github.com>
2025-05-14 15:45:20 +0700

    Improve ObjectPooling

--- Assets/Scripts/Enemy/DestroyOutOfBound.cs
+++ Assets/Scripts/Enemy/DestroyOutOfBound.cs
@@ -1,10 +1,12 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DestroyOutOfBound : MonoBehaviour
 {
-    // [1] declare a private float variable to store the top bound
     private float topBound = 30;
     private float lowerBound = -10;
+    [SerializeField] private bool isBoss = false;
 
     // Update is called once per frame
     void Update()
@@ -12,11 +14,20 @@ public class DestroyOutOfBound : MonoBehaviour
         // [2] if the object goes out of the top bound
         if (transform.position.z > topBound)
         {
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
         else if (transform.position.z < lowerBound)
         {
-            Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (isBoss)
+        {
+            gameObject.SetActive(false);
+            SceneManager.LoadScene(0);
         }
     }
 }
